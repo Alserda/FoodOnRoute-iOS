359375948f48db6201045ca4694b58d007e6dfca
Peter Alserda <peteralserda@hotmail.com>
2015-12-16 00:05:45 +0100

    upper background added to product view

--- FoodOnRoute/UserInterface/ProductViewController.swift
+++ FoodOnRoute/UserInterface/ProductViewController.swift
@@ -13,24 +13,43 @@ class ProductViewController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         self.view.backgroundColor = foodOnRouteColor.lightBlue
+        setNavigationbarAppearance()
+        addUpperBackground()
+
+    }
+    
+    func addUpperBackground() {
+        let upperBackground: UIImageView = UIImageView(image: UIImage(named: "UpperBackground"))
+        upperBackground.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(upperBackground)
+        
+        upperBackground.topAnchor.constraintEqualToAnchor(self.topLayoutGuide.bottomAnchor, constant: 6).active = true
+        upperBackground.rightAnchor.constraintEqualToAnchor(self.view.rightAnchor, constant: 0).active = true
+    
+        
+        upperBackground.constrainToSize(CGSize(width: 248, height: 88))
+    }
+    
+    
+    func setNavigationbarAppearance() {
         UIApplication.sharedApplication().statusBarStyle = .LightContent
         
         self.navigationController?.navigationBar.barTintColor = foodOnRouteColor.lightGreen
         let image = UIImage(named: "BarButtonItem")!.imageWithRenderingMode(.AlwaysOriginal)
         let button = UIBarButtonItem(image: image, style: UIBarButtonItemStyle.Done, target: self, action: "goBack")
         self.navigationItem.leftBarButtonItem = button
-
+        
         if let navigationController = self.navigationController {
             
             let navigationBar = navigationController.navigationBar
             let navBorder: UIView = UIView(frame: CGRectMake(0, navigationBar.frame.size.height - 1, navigationBar.frame.size.width, 3))
-            // Set the color you want here
             navBorder.backgroundColor = foodOnRouteColor.shadowGreen
             navBorder.opaque = true
             self.navigationController?.navigationBar.addSubview(navBorder)
         }
-
     }
     
     override func viewWillDisappear(animated: Bool) {
